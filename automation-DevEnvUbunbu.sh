6c50e158b65b87ca8cad1eb4697a54b1d06c9562
Log_N <Log-N@outlook.com>
2021-04-22 16:41:05 -0230

    Update automation-DevEnvUbunbu.sh

--- automation-DevEnvUbunbu.sh
+++ automation-DevEnvUbunbu.sh
@@ -6,19 +6,21 @@ then
  exit 1
 fi
 
-# Ensure system is up to date
+# update
 sudo apt-get update -y 
 
-# Upgrade the system
+# Upgrade
 sudo apt-get upgrade -y
 
-# Install OpenSSH
-sudo apt-get install openssh-server netdata zip p7zip-full net-tools speedtest-cli build-essential checkinstall libssl-dev python3-pip golang gnome-disk-utility neofetch -y
+# Install ur packs essentials
+sudo apt-get install openssh-server netdata zip p7zip-full net-tools speedtest-cli build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk checkinstall libssl-dev python3-pip golang gnome-disk-utility neofetch -y
+
 
 # Enable Firewall
 sudo ufw enable 
 
-# configure the firewallsudo ufw allow OpenSSH
+# configure the firewall 
+sudo ufw allow OpenSSH
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow OpenSSH
@@ -46,7 +48,7 @@ maxretry = 4
 " >> /etc/fail2ban/jail.local
 
 
-# Install nodejs v14.15.4
+# Install nodejs v14XXX
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt update -y
 sudo apt install node -y
@@ -57,7 +59,6 @@ sudo apt install npm -y
 # Install nvm pack
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
-
 # Install Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
@@ -67,15 +68,21 @@ sudo apt install --no-install-recommends yarn -y
 # Install clojure pack
 sudo apt install clojure -y
 
-# Install sdkman pack
+# Install sdkman pack - sdkman its java tooling
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"  
 
-# Isntall VCode
+# Install elixir
+sudo apt install install esl-erlang erlang-debugger erlang-dev elixir -y
+
+# Install VCode
 sudo snap install code --classic -y
 
+# Install Insomnia
+sudo snap install insomnia -y
 
-# Docker option install 
+
+# Docker optionional install 
 echo "
 ######################################################################################################
 Do you want to install docker? If so type y / If you dont want to install enter n
